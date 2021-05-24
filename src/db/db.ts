b6ac94d5b25ea64d024c9ba35359d82084f1054f
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-24 18:38:12 +0530

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
+import * as constants from '../utility/constants';
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'tryagain',
-    database: 'edms_database',
-    host: 'localhost',
-    port: 5432
+    user: constants.DB_USERNAME,
+    password: constants.DB_PASSWORD,
+    database: constants.DB_DBNAME,
+    host: constants.DB_HOST,
+    port: constants.DB_PORT
 });
 
 export default pool;
