eb625a3e6cfdf29e6c9565cf0188c13cfea9ba25
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-24 18:38:08 +0530

    Update dept.ts

--- src/controllers/dept.ts
+++ src/controllers/dept.ts
@@ -1,31 +1,30 @@
 import pool from '../db/db';
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
+import * as constants from '../utility/constants';
 
 // Class Based Controller
 class DeptController {
-    async createDept (req: Request, res: Response) {
+    async createDepartment (req: Request, res: Response) {
         try {
-            const newDept = await pool.query('INSERT INTO dept(dept_name) VALUES($1) RETURNING *', [req.body.deptName]);
-
+            const newDept = await pool.query(constants.DEPT_INSERT_QUERY, [req.body.deptName]);
             res.send(newDept.rows[0]);
         } catch (error) {   
             res.status(400).json({ error });
         }
     }
 
-    async getAllDept (req: Request, res: Response) {
+    async searchDepartment (req: Request, res: Response) {
         try {
-            const depts = await pool.query('SELECT * FROM dept');
-    
-            res.send(depts.rows);
-        } catch (error) {
-            res.status(400).json({ error });
-        }
-    }
-    
-    async getDept (req: Request, res: Response) {
-        try {
-            const depts = await pool.query('SELECT * FROM dept where dept_id = $1', [req.params.id]);
+            const query = constants.DEPT_SELECTION_QUERY;
+            let searchQuery = '';
+
+            if (req.query.searchBy) {
+
+                const searchFilter = req.query.searchBy.toString().split(':');
+                searchQuery = ` WHERE ${searchFilter[0]} ${searchFilter[1]} ${searchFilter[2]}`;
+            }
+
+            const depts = await pool.query(query + searchQuery);
     
             res.send(depts.rows);
         } catch (error) {
@@ -33,11 +32,16 @@ class DeptController {
         }
     }
 
-    async deleteDept (req: Request, res: Response) {
+    async deleteDepartment (req: Request, res: Response) {
         try {
-            await pool.query('DELETE FROM dept WHERE dept_id = $1', [req.params.id]);
+            const emp = await pool.query(constants.DEPT_SELECTION_QUERY + " WHERE dept_id = $1", [req.params.id]);
+            if (emp.rowCount == 0) {
+                return res.status(400).json({ msg: 'DEPARTMENT NOT FOUND' });
+            }
+
+            await pool.query(constants.DEPT_DELETION_QUERY, [req.params.id]);
     
-            res.send('Department DELETED');
+            res.json({ msg: 'DEPARTMENT DELETED' });
         } catch (error) {
             res.status(400).json({ error });
         }
